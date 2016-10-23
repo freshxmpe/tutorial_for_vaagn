a07d4ce7029bda6350a2d068d98e4730f85fac52
gugfug <gugfug1@gmail.com>
2016-10-23 12:35:25 +0300

    add func tutor

---
+++ func.py
@@ -0,0 +1,11 @@
+def func(argument1, argument2):
+    print(argument1) # первый аргумент
+    print(argument2) # второй аргумент
+
+# попробуй повызывать функцию с разными параметрами
+func('awesome', 'stuff')
+
+
+# чтобы терминал не закрывался
+if __name__ == "__main__":
+    input()
